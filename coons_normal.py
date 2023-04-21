7960cf183eb4430828307292fd4a0820bf04add9
THLsword <604300468@qq.com>
2023-04-21 08:52:23 +0800

    add test code

--- coons_normal.py
+++ coons_normal.py
@@ -1,47 +1,158 @@
-# wirtten by bing:
-# def normal(s, t, u, v, patches):
-#     # Compute partial derivatives
-#     du = np.array([1, 0])
-#     dv = np.array([0, 1])
-#     point = coons_points(u, v, patches)
-#     x_u = du @ coons_points(u, v, patches)
-#     x_v = dv @ coons_points(u, v, patches)
-#     # Compute cross product
-#     n = np.cross(x_u, x_v)
-#     # Normalize
-#     n /= np.linalg.norm(n)
-#     return n
-
-
+from sympy import symbols, Matrix, diff
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Define the Coons patch function
-def coons_patch(u, v):
-    # Define the control points
-    p00 = np.array([0, 0, 0])
-    p01 = np.array([0, 1, 0])
-    p10 = np.array([1, 0, 0])
-    p11 = np.array([1, 1, 1])
-    
-    # Calculate the blending functions
-    f1 = (1 - u) * p00 + u * p10
-    f2 = (1 - u) * p01 + u * p11
-    g1 = (1 - v) * p00 + v * p01
-    g2 = (1 - v) * p10 + v * p11
-    
-    # Calculate the surface point
-    s = (1 - u) * (1 - v) * p00 + u * (1 - v) * p10 + (1 - u) * v * p01 + u * v * p11
-    
-    # Calculate the partial derivatives
-    du = (1 - v) * (f2 - f1) + v * (g2 - g1)
-    dv = (1 - u) * (g2 - g1) + u * (f2 - f1)
-    
-    # Calculate the normal vector
-    n = np.cross(du, dv)
-    
-    return n
+def bezier_sample(t, params):
+    """Sample points from cubic Bezier curves defined by params at t values."""
+    A = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+    [-7, 7, 0, 0, 0, 0, 0, 0],
+    [21, -42, 21, 0, 0, 0, 0, 0],
+    [-35, 105, -105, 35, 0, 0, 0, 0],
+    [35, -140, 210, -140, 35, 0, 0, 0],
+    [-21, 105, -210, 210, -105, 21, 0, 0],
+    [7, -42, 105, -140, 105, -42, 7, 0],
+    [-1, 7, -21, 35, -35, 21, -7, 1]
+    ])
+    t = np.power(t, np.array([0, 1, 2, 3, 4, 5, 6, 7]))  # [n_samples, 4]
 
-# Call the function with the desired values of u and v
-u = 0.5
-v = 0.5
-coons_patch(u, v)
+    points = t @ A @ params  # [..., n_samples, 3]
+    return points
+
+def coons_points(s, t, patches):
+    """Sample points from Coons patch defined by params at s, t values.
+
+    params -- [..., 12, 3]
+    """
+    # sides = [patches[..., :4, :], patches[..., 3:7, :],
+    #          patches[..., 6:10, :], patches[..., [9, 10, 11, 0], :]]
+    # corners = [patches[..., [0], :], patches[..., [3], :],
+    #            patches[..., [9], :], patches[..., [6], :]]
+
+    sides = [patches[:8, :], patches[7:15, :],
+             patches[14:22, :], patches[[21, 22, 23, 24, 25, 26, 27, 0], :]] 
+    corners = [patches[[0], :], patches[[7], :],
+               patches[[21], :], patches[[14], :]]
+           
+    B = corners[0] * (1-s) * (1-t) + corners[1] * s * (1-t) + \
+        corners[2] * (1-s) * t + corners[3] * s * t  # [..., n_samples, 3]
+
+    Lc = bezier_sample(s, sides[0]) * (1-t) + bezier_sample(1-s, sides[2]) * t
+    Ld = bezier_sample(t, sides[1]) * s + bezier_sample(1-t, sides[3]) * (1-s)
+    
+    return Lc + Ld - B
+
+# Generate random control points
+P0 = np.array([1*2, 1, 1])
+P1 = np.array([2*2, 1.2, 1.2])
+P2 = np.array([3*2, 1, 1.3])
+P3 = np.array([4*2, 1.2, 1.4])
+P4 = np.array([5*2, 1, 1.3])
+P5 = np.array([6*2, 1.2, 1.2])
+P6 = np.array([8*2, 1, 1])
+P7 = np.array([9*2, 1.2, 1])
+
+P8 = np.array([9.5*2, 1, 1.1])
+P9 = np.array([9*2, 2, 1.5])
+P10 = np.array([9.5*2, 3, 0.8])
+P11 = np.array([9*2, 4, 0.6])
+P12 = np.array([9.5*2, 5, 0.9])
+P13 = np.array([9*2, 6, 1])
+P14 = np.array([9.5*2, 7, 1.2])
+
+P15 = np.array([9*2, 7.3, 1.2])
+P16 = np.array([8*2, 7, 1.3])
+P17 = np.array([7*2, 7.3, 0.8])
+P18 = np.array([6*2, 7, 0.5])
+P19 = np.array([5*2, 7.3, 0.5])
+P20 = np.array([4*2, 7, 0.8])
+P21 = np.array([2*2, 7.3, 1])
+
+P22 = np.array([2*2, 7, 1])
+P23 = np.array([2*2, 6, 1.3])
+P24 = np.array([2*2, 5, 1.3])
+P25 = np.array([2*2, 4, 0.7])
+P26 = np.array([2*2, 3, 0.7])
+P27 = np.array([2*2, 2, 1])
+
+params = np.stack([P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, P23, P24, P25, P26, P27], axis=0)
+
+# Sample points on the Bezier curve
+t = np.linspace(0, 1, 100).reshape((100, 1))
+s = np.linspace(0, 1, 100).reshape((100, 1))
+
+
+A = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+    [-7, 7, 0, 0, 0, 0, 0, 0],
+    [21, -42, 21, 0, 0, 0, 0, 0],
+    [-35, 105, -105, 35, 0, 0, 0, 0],
+    [35, -140, 210, -140, 35, 0, 0, 0],
+    [-21, 105, -210, 210, -105, 21, 0, 0],
+    [7, -42, 105, -140, 105, -42, 7, 0],
+    [-1, 7, -21, 35, -35, 21, -7, 1]
+    ])
+t_power = np.power(t, np.array([0, 1, 2, 3, 4, 5, 6, 7]))  # [n_samples, 4]
+s_power = np.power(s, np.array([0, 1, 2, 3, 4, 5, 6, 7]))  # [n_samples, 4]
+
+points = t @ A @ params  # [..., n_samples, 3]
+
+
+sides = [patches[:8, :], patches[7:15, :],
+             patches[14:22, :], patches[[21, 22, 23, 24, 25, 26, 27, 0], :]] 
+corners = [patches[[0], :], patches[[7], :],
+            patches[[21], :], patches[[14], :]]
+        
+B = corners[0] * (1-s) * (1-t) + corners[1] * s * (1-t) + \
+    corners[2] * (1-s) * t + corners[3] * s * t  # [..., n_samples, 3]
+
+Lc = bezier_sample(s, sides[0]) * (1-t) + bezier_sample(1-s, sides[2]) * t
+Ld = bezier_sample(t, sides[1]) * s + bezier_sample(1-t, sides[3]) * (1-s)
+
+P = Lc + Ld - B
+
+
+
+
+
+
+
+
+
+
+# curve = bezier_sample(t, params)
+# curve1 = coons_points(np.linspace(0, 0, 100).reshape((100, 1)),np.linspace(0, 1, 100).reshape((100, 1)),params)
+# curve2 = coons_points(np.linspace(0, 1, 100).reshape((100, 1)),np.linspace(1, 1, 100).reshape((100, 1)),params)
+# curve3 = coons_points(np.linspace(1, 1, 100).reshape((100, 1)),np.linspace(0, 1, 100).reshape((100, 1)),params)
+# curve4 = coons_points(np.linspace(0, 1, 100).reshape((100, 1)),np.linspace(0, 0, 100).reshape((100, 1)),params)
+
+# curve5 = coons_points(np.linspace(0, 1, 100).reshape((100, 1)),np.linspace(0.25, 0.25, 100).reshape((100, 1)),params)
+# curve6 = coons_points(np.linspace(0, 1, 100).reshape((100, 1)),np.linspace(0.5, 0.5, 100).reshape((100, 1)),params)
+# curve7 = coons_points(np.linspace(0, 1, 100).reshape((100, 1)),np.linspace(0.75, 0.75, 100).reshape((100, 1)),params)
+
+
+
+
+
+
+
+
+
+
+
+
+# Plot the curve
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(curve1[:, 0], curve1[:, 1], curve1[:, 2])
+ax.plot(curve2[:, 0], curve2[:, 1], curve2[:, 2])
+ax.plot(curve3[:, 0], curve3[:, 1], curve3[:, 2])
+ax.plot(curve4[:, 0], curve4[:, 1], curve4[:, 2])
+
+ax.plot(curve5[:, 0], curve5[:, 1], curve5[:, 2])
+ax.plot(curve6[:, 0], curve6[:, 1], curve6[:, 2])
+ax.plot(curve7[:, 0], curve7[:, 1], curve7[:, 2])
+
+
+
+ax.scatter(params[:, 0], params[:, 1], params[:, 2], c='r')
+plt.show()
--- sympy_test.py
+++ sympy_test.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# Define symbols
-x, y = symbols('x y')
+# # Define symbols
+# x, y = symbols('x y')
 
-# Define matrix
-A = Matrix([[x**2, x*y], [x*y, y**2]])
+# # Define matrix
+# A = Matrix([[x**2, x*y], [x*y, y**2]])
 
-# Take partial derivative of matrix with respect to x
-dAdx = A.applyfunc(lambda a: diff(a, x))
+# # Take partial derivative of matrix with respect to x
+# dAdx = A.applyfunc(lambda a: diff(a, x))
 
-# Print result
-print(dAdx)
+# # Print result
+# print(dAdx)
 
 A = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
     [-7, 7, 0, 0, 0, 0, 0, 0],
@@ -25,16 +25,42 @@ A = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
     [7, -42, 105, -140, 105, -42, 7, 0],
     [-1, 7, -21, 35, -35, 21, -7, 1]
     ])
-t = np.power(t, np.array([0, 1, 2, 3, 4, 5, 6, 7]))  # [n_samples, 4]
-points = t @ A @ params  # [..., n_samples, 3]
+# t = np.power(t, np.array([0, 1, 2, 3, 4, 5, 6, 7]))  # [n_samples, 4]
+# points = t @ A @ params  # [..., n_samples, 3]
 
-sides = [patches[:8, :], patches[7:15, :],
-             patches[14:22, :], patches[[21, 22, 23, 24, 25, 26, 27, 0], :]] 
-corners = [patches[[0], :], patches[[7], :],
-            patches[[21], :], patches[[14], :]]
+# sides = [patches[:8, :], patches[7:15, :],
+#              patches[14:22, :], patches[[21, 22, 23, 24, 25, 26, 27, 0], :]] 
+# corners = [patches[[0], :], patches[[7], :],
+#             patches[[21], :], patches[[14], :]]
         
-B = corners[0] * (1-s) * (1-t) + corners[1] * s * (1-t) + corners[2] * (1-s) * t + corners[3] * s * t 
-Lc = (s @ A @ sides[0])*(1-t) + ((1-s) @ A @ sides[2])*(t)
-Ld = (t @ A @ sides[1])*(s) + ((1-t) @ A @ sides[3])*(1-s)
-P = Lc + Ld - B
+# B = corners[0] * (1-s) * (1-t) + corners[1] * s * (1-t) + corners[2] * (1-s) * t + corners[3] * s * t 
+# Lc = (s @ A @ sides[0])*(1-t) + ((1-s) @ A @ sides[2])*(t)
+# Ld = (t @ A @ sides[1])*(s) + ((1-t) @ A @ sides[3])*(1-s)
+# P = Lc + Ld - B
+t = np.linspace(0, 1, 5).reshape((5, 1))
+t = np.power(t, np.array([0, 1]))  # [n_samples, 4]
+# TA = t @ A
+s = np.linspace(0, 1, 5).reshape((5, 1))
+# TA = TA * s
+print(t)
+print(s.shape)
+print(t * s)
+
+Ss, St = symbols('s t')
+St =  Matrix(t)
+St = Matrix([St**i for i in range(8)])
+print(St.shape)
+print(St)
+
+s, t = symbols('s t')
+s = Matrix([s]*144)
+t = Matrix([t]*144)
+
+# sides[0]).shape = (8,3)
+Lc = (s @ A @ sides[0])*(1-t)
+
+# Derivative of Lc w.r.t s
+Lc_s = Lc.jacobian(s)
+
+t = Matrix([t**i for i in range(8)])
 
